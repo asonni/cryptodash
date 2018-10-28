7fad67e099419e75d423aa71df39b3253a5e3d53
Aladdin Sonni <aladdin.sonni@gmail.com>
2018-10-28 23:21:03 +0200

    Updates

---
+++ precache-manifest.d934f89b402fe87fadeb930692b60ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4d87cd26acbefad9ac5",
+    "url": "/asonni/cryptodash/static/js/runtime~main.f4d87cd2.js"
+  },
+  {
+    "revision": "3f7890a2d54865988ab0",
+    "url": "/asonni/cryptodash/static/js/main.3f7890a2.chunk.js"
+  },
+  {
+    "revision": "fb58d5dd9b12eaa49955",
+    "url": "/asonni/cryptodash/static/js/1.fb58d5dd.chunk.js"
+  },
+  {
+    "revision": "3f7890a2d54865988ab0",
+    "url": "/asonni/cryptodash/static/css/main.a17e13e7.chunk.css"
+  },
+  {
+    "revision": "02c0462ddd8abf8101b82715b93dc5f1",
+    "url": "/asonni/cryptodash/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/asonni/cryptodash/precache-manifest.d934f89b402fe87fadeb930692b60ae7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/asonni/cryptodash/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
